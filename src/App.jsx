2a37b23e117a082f9f5210738c96cd096f5e2892
herish05 <herishgarg@gmail.com>
2025-02-08 16:22:39 +0530

    User signIn ,Login and navbar,footer updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Layout from './Layout'
 import Home from './components/Home'
+import SignIn from './components/SignIn'
+import Login from './components/Login'
+import Not_found from './components/Not_found'
 
 const App = () => {
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<Home/>} />
-        <Route path="/about" element={<h1>About</h1>} />
-        <Route path="/pricing" element={<h1>Pricing</h1>} />
+        <Route path="/signin" element={<SignIn/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="*" element={<Not_found/>}/>
       </Routes>
     </Layout>
   )
